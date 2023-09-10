f568409245f4c3f0557b45a810722412956c36f2
Bongomin Daniel <bongomindaniel@gmail.com>
2023-09-10 12:55:50 +0300

    register account app

---
+++ account/admin.py
@@ -0,0 +1,20 @@
+from django.contrib import admin
+from account.models import Account
+from userauth.models import User
+from import_export import resources
+from import_export.admin import ImportExportModelAdmin
+
+class AccountAdminModel(ImportExportModelAdmin):
+    list_editable =['account_status', 'account_balance']
+    list_display = ['user', 'account_number', 'account_status', 'account_balance']
+    list_filter = ['account_status']
+
+
+# class KYCAdmin(ImportExportModelAdmin):
+#     model: Account
+#     search_fields = ['full_name']
+#     list_display = ['user', 'full_name']
+
+
+admin.site.register(Account, AccountAdminModel)
+
