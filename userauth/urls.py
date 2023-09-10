d67be5b9ceb0decdcc96b8ca92b1520808beeeb7
Bongomin Daniel <bongomindaniel@gmail.com>
2023-09-10 08:31:41 +0300

    add logout url

--- userauth/urls.py
+++ userauth/urls.py
@@ -4,5 +4,6 @@ from userauth import views
 app_name = "userauth"
 
 urlpatterns = [
-    path("register/", views.RegisterUserView, name="register-user")
+    path("register/", views.RegisterUserView, name="register-user"),
+    path("logout/", views.logoutUserView, name="logout-user")
 ]
