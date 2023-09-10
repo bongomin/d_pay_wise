f2c332b3aa962ceb7cf5298df996c376b2cecb23
Bongomin Daniel <bongomindaniel@gmail.com>
2023-09-10 09:35:57 +0300

    login url

--- userauth/urls.py
+++ userauth/urls.py
@@ -5,5 +5,6 @@ app_name = "userauth"
 
 urlpatterns = [
     path("register/", views.RegisterUserView, name="register-user"),
+     path("login/", views.LoginUserView, name="login-user"),
     path("logout/", views.logoutUserView, name="logout-user")
 ]
