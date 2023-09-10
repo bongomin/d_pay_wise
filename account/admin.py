a3771d733dd3ae79cbcc761f388b427086c62d4e
Bongomin Daniel <bongomindaniel@gmail.com>
2023-09-10 15:09:14 +0300

    register KYC model

--- account/admin.py
+++ account/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from account.models import Account
+from account.models import Account,KYC
 from userauth.models import User
 from import_export import resources
 from import_export.admin import ImportExportModelAdmin
@@ -10,11 +10,13 @@ class AccountAdminModel(ImportExportModelAdmin):
     list_filter = ['account_status']
 
 
-# class KYCAdmin(ImportExportModelAdmin):
-#     model: Account
-#     search_fields = ['full_name']
-#     list_display = ['user', 'full_name']
+class KYCAdmin(ImportExportModelAdmin):
+    model: Account
+    search_fields = ['full_name']
+    list_display = ['user', 'full_name','mobile','country', 'city']
 
 
 admin.site.register(Account, AccountAdminModel)
+admin.site.register(KYC, KYCAdmin)
+
 
