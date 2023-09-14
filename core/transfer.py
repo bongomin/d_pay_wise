a3804a7d55d1d82c1134c8084465bc754e90fef9
Bongomin Daniel <bongomindaniel@gmail.com>
2023-09-14 11:23:30 +0300

    show the searched account AmountTransfer

--- core/transfer.py
+++ core/transfer.py
@@ -2,6 +2,7 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 from account.models import Account
 from django.db.models import Q
+from django.contrib import messages
 
 
 @login_required
@@ -20,3 +21,14 @@ def search_user_by_account_number(request):
         "query": query
     }
     return render(request, "transfer/searchUserByAccountNumber.html", context)
+
+def AmountTransfer(request, account_number):
+    try:
+        account = Account.objects.get(account_number=account_number)
+    except:
+        messages.warning(request,"Account does not exists")
+        return redirect("core:search-account")
+    context = {
+        "account": account
+    }
+    return render(request,"transfer/amount-transfer.html",context)
