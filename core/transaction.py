8c3e511b13099c3efa0d5cccc143ddc7f36ac2da
Bongomin Daniel <bongomindaniel@gmail.com>
2023-09-19 13:13:07 +0300

    add ky and account info

--- core/transaction.py
+++ core/transaction.py
@@ -1,6 +1,6 @@
 from core.models import Transaction
 from django.shortcuts import render,redirect
-from account.models import Account
+from account.models import Account,KYC
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.shortcuts import render
@@ -8,10 +8,14 @@ from django.shortcuts import render
 def transaction_lists(request):
     sender_transaction = Transaction.objects.filter(sender=request.user).order_by("-id")
     receiver_transaction = Transaction.objects.filter(receiver=request.user).order_by("-id")
+    kyc = KYC.objects.get(user=request.user)
+    account = Account.objects.get(user=request.user)
 
     context = {
         "sender_transaction": sender_transaction,
-        "receiver_transaction": receiver_transaction
+        "receiver_transaction": receiver_transaction,
+        "account": account,
+        "kyc": kyc
     }
 
     return render(request, "transaction/transaction-list.html", context)
