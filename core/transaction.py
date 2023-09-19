c12c0ef2d1bc4cb5257d19ed3f511404a86aacee
Bongomin Daniel <bongomindaniel@gmail.com>
2023-09-19 12:45:32 +0300

    Transaction list

--- core/transaction.py
+++ core/transaction.py
@@ -7,11 +7,11 @@ from django.shortcuts import render
 
 def transaction_lists(request):
     sender_transaction = Transaction.objects.filter(sender=request.user).order_by("-id")
-    reciever_transaction = Transaction.objects.filter(receiver=request.user).order_by("-id")
+    receiver_transaction = Transaction.objects.filter(receiver=request.user).order_by("-id")
 
     context = {
-        "sender": sender_transaction,
-        "receiver": reciever_transaction
+        "sender_transaction": sender_transaction,
+        "receiver_transaction": receiver_transaction
     }
 
     return render(request, "transaction/transaction-list.html", context)
