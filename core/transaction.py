89593dc7ba01b4a23a5b15611df4a814b11a4f97
Bongomin Daniel <bongomindaniel@gmail.com>
2023-09-20 20:42:04 +0300

    fix:show transaction_lists

--- core/transaction.py
+++ core/transaction.py
@@ -3,18 +3,23 @@ from django.shortcuts import render,redirect
 from account.models import Account,KYC
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 @login_required
 def transaction_lists(request):
-    sender_transaction = Transaction.objects.filter(sender=request.user).order_by("-id")
-    receiver_transaction = Transaction.objects.filter(receiver=request.user).order_by("-id")
-    kyc = KYC.objects.get(user=request.user)
-    account = Account.objects.get(user=request.user)
+    sender_transaction = Transaction.objects.filter(sender=request.user, transaction_type="transfer").order_by("-id")
+    receiver_transaction = Transaction.objects.filter(receiver=request.user, transaction_type="transfer").order_by("-id")
+    request_sender_transaction = Transaction.objects.filter(sender=request.user, transaction_type="request")
+    request_receiver_transaction = Transaction.objects.filter(receiver=request.user, transaction_type="request")
+
+    kyc = get_object_or_404(KYC, user=request.user)
+    account = get_object_or_404(Account, user=request.user)
 
     context = {
         "sender_transaction": sender_transaction,
         "receiver_transaction": receiver_transaction,
+        "request_sender_transaction": request_sender_transaction,
+        "request_receiver_transaction": request_receiver_transaction,
         "account": account,
         "kyc": kyc
     }
