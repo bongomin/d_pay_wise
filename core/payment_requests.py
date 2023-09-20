723f1d19c68466d97047bb9e1402bd7fc92ced2e
Bongomin Daniel <bongomindaniel@gmail.com>
2023-09-20 11:31:19 +0300

    AmountRequest logic andAmountRequestProcess Logic

--- core/payment_requests.py
+++ core/payment_requests.py
@@ -27,3 +27,50 @@ def SearchUserRequest(request):
             }
 
     return render(request, "payment_request/search-users.html", context)
+
+
+def AmountRequest(request,account_number):
+    account = Account.objects.get(account_number=account_number)
+    kyc = KYC.objects.get(user=request.user)
+    context = {
+        "account": account,
+        "kyc":kyc
+    }
+
+    return render(request, "payment_request/amount-request.html", context)
+
+def AmountRequestProcess(request,account_number):
+    account = Account.objects.get(account_number=account_number)
+
+
+    sender = request.user
+    receiver = account.user
+
+    sender_account = request.user.account
+    receiver_account = account
+
+    if request.method == "POST":
+        amount = request.POST.get("amount-request")
+        description = request.POST.get("description")
+
+        new_request = Transaction.objects.create(
+            user=request.user,
+            amount=amount,
+            description=description,
+            sender=sender,
+            receiver=receiver,
+            sender_account=sender_account,
+            receiver_account=receiver_account,
+            status="requested",
+            transaction_type="request"
+        )
+
+        new_request.save()
+        transaction_id = new_request.transaction_id
+        return redirect("core:request-confirmation",account.account_number, transaction_id)
+
+    else:
+        messages.warning(request, "Error Occured,Try again later")
+        return redirect("core:dashboard")
+
+
