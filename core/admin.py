8bd8b8d0e783d0879c5c098aa2e3cd4cf4d4ba1f
Bongomin Daniel <bongomindaniel@gmail.com>
2023-09-23 05:32:06 +0300

    feat:Model:creditCard model

--- core/admin.py
+++ core/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
-from core.models import Transaction
+from core.models import Transaction,CreditCard
 
 class TransactionAdmin(admin.ModelAdmin):
     list_display = ['user', 'amount', 'status', 'transaction_type', 'receiver', 'sender']
     list_editable = ['amount', 'status', 'transaction_type', 'receiver', 'sender']
 
+class CreditCardAdmin(admin.ModelAdmin):
+    list_display = ['user', 'amount', 'card_status', 'card_type', 'month', 'year', 'cvv']
+    list_editable = ['amount', 'card_status', 'card_type', 'month', 'year', 'cvv']
+
+admin.site.register(CreditCard, CreditCardAdmin)
 admin.site.register(Transaction, TransactionAdmin)
