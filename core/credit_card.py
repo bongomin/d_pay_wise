8c14ba4b30fb40fda8a851be2898802e0e0cd4c2
Bongomin Daniel <bongomindaniel@gmail.com>
2023-09-23 15:15:27 +0300

    Add funds to card

--- core/credit_card.py
+++ core/credit_card.py
@@ -4,6 +4,7 @@ from core.models import CreditCard
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from account.models import Account
+from decimal import Decimal
 
 
 def credit_card_details(request, card_id):
@@ -18,3 +19,33 @@ def credit_card_details(request, card_id):
         "kyc": kyc
     }
     return render(request, "credit_card/card-detail.html", context)
+
+
+# Add money to my Credit Account
+def fund_credit_card(request, card_id):
+    account = Account.objects.get(user=request.user)
+    credic_card = CreditCard.objects.get(card_id=card_id, user=request.user)
+    kyc = KYC.objects.get(user=request.user)
+    user_account = Account.objects.get(user=request.user)
+
+    if request.method == "POST" :
+        amount = request.POST.get("funding_amount")
+        amount = Decimal(amount)
+        if amount < account.account_balance:
+            # deduct money form persal account to add or fund to the Credit Card
+            account.account_balance -= Decimal(amount)
+            account.save()
+
+            #  add the deducted amount from the account and top it to the card
+            credic_card.amount += Decimal(amount)
+            credic_card.save()
+
+            messages.success(request,"Credit card funding successfull")
+            return redirect("core:credit-card-details", credic_card.card_id)
+
+        else:
+            messages.success(request,"Insufficient funds, kindly top up and try again ")
+            return redirect("core:credit-card-details", credic_card.card_id)
+
+
+
