e18bfb9c1e8e25c90958599e9c88eb57aeb0c42d
Bongomin Daniel <bongomindaniel@gmail.com>
2023-09-29 16:34:10 +0300

    add .env file

--- daniel_pay_wise/settings.py
+++ daniel_pay_wise/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '4f135^0$e%yz6*b7o^*m5w!-(1d2e-z&p76k&r*nn+_e7w4ai2'
+SECRET_KEY =os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
